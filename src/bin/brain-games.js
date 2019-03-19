2fbf8db644cedc30761d50df247e0eb1c332cfaa
mikhail.a <mkhlslkhnv@gmail.com>
2019-03-19 17:34:07 +1000

    added functionality

--- src/bin/brain-games.js
+++ src/bin/brain-games.js
@@ -1,2 +1,7 @@
-#! /usr/bin/node
-console.log('Welcome to the Brain Games!');
+#! /usr/bin/env node
+
+import {greeting} from '..';
+
+console.log('Welcome to the Brain Games!', '\n');
+greeting();
+
