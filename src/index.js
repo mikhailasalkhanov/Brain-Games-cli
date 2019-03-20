826ebd2a458141819cddc78a9b07695822124a86
mikhail.a <mkhlslkhnv@gmail.com>
2019-03-20 23:24:58 +1000

    fix syntax

--- src/index.js
+++ src/index.js
@@ -1,6 +1,6 @@
 import readlineSync from 'readline-sync';
 
-export const greeting = () => {
+export default () => {
   const playerName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${playerName}!`);
   return playerName;
