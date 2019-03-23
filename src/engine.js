13d5de7889fd23bd9c8b197b4d2cf6739d6848a9
mikhail.a <mkhlslkhnv@gmail.com>
2019-03-23 17:29:06 +1000

    search operator by key, not by index

--- src/engine.js
+++ src/engine.js
@@ -1,13 +1,21 @@
 import readlineSync from 'readline-sync';
 import { car, cdr } from 'hexlet-pairs';
 
+export const rounds = 3;
+
+export const randomInt = (min, max) => {
+  let rand = min + Math.random() * (max + 1 - min);
+  rand = Math.floor(rand);
+  return rand;
+};
+
 export default (gameDescription, questionsAndAnswers) => {
   console.log('Welcome to the Brain Games');
   console.log(gameDescription, '\n');
   const playerName = readlineSync.question('May I have your name? ');
   console.log(`Hello, ${playerName}!`);
 
-  for (let attempt = 0; attempt < 3; attempt += 1) {
+  for (let attempt = 0; attempt < rounds; attempt += 1) {
     const question = car(questionsAndAnswers[attempt]);
     const correctAnswer = cdr(questionsAndAnswers[attempt]);
 
