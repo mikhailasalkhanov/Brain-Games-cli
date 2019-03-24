1448ebf3d2feb8f2d3eeda761f9a731886a36579
mikhail.a <mkhlslkhnv@gmail.com>
2019-03-24 15:05:47 +1000

    reduce method changed to join method

--- src/games/progression.js
+++ src/games/progression.js
@@ -4,12 +4,10 @@ import randomInt from '../utils';
 
 
 const description = 'What number is missing in the progression?';
-
-const getQuestion = (acc, current) => `${acc} ${current}`;
+const progressionLen = 10;
 
 const generateProgression = () => {
   const progression = [];
-  const progressionLen = 10;
   const firstElem = randomInt(1, 100);
   const step = randomInt(1, 10);
 
@@ -26,11 +24,12 @@ export default () => {
 
   for (let i = 0; i < rounds; i += 1) {
     const progression = generateProgression();
-    const hiddenElemPosition = randomInt(0, 9);
+    const positionHiddenElem = randomInt(0, progressionLen - 1);
+    const hiddenElem = progression[positionHiddenElem];
 
-    const answer = `${progression[hiddenElemPosition]}`;
-    progression[hiddenElemPosition] = '..';
-    const question = progression.reduce(getQuestion);
+    const answer = `${hiddenElem}`;
+    progression[positionHiddenElem] = '..';
+    const question = progression.join(' ');
 
     gameData.push(cons(question, answer));
   }
